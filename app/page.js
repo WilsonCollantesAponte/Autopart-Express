dca7f3a55a3c82f3d279fd192dd3771296ea97f4
Aborio <aguborio18@gmail.com>
2023-10-23 15:28:55 -0300

    rwerw

--- app/page.js
+++ app/page.js
@@ -19,7 +19,7 @@ export default function Home() {
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
-              className="dark:invert"
+              className="dark:invertdfgdfgd"
               width={100}
               height={24}
               priority
