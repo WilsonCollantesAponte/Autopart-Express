57b69c9f8c2c0702b922cfc8d1683498531f4252
maxi-pereyra <maxiabelpereyra@hotmail.com.ar>
2023-10-24 17:52:52 -0300

    delete-put-product

--- app/product/api/route.js
+++ app/product/api/route.js
@@ -1,12 +1,51 @@
 import DataBaseInteraction from "@/prisma";
-import { NextResponse } from "next/server";
+import { NextResponse , NextRequest} from "next/server";
 
 
 export async function GET(){
     try {
         const products = await DataBaseInteraction.Product.findMany()
-        return NextResponse.json({products})
+        return NextResponse.json(products , {status: 200})
     } catch (error) {
-        return NextResponse.json({message: error.message}, {status: 500,});   
+        return NextResponse.json({message: error.message}, {status: 500});   
     }
 }
+
+
+export const PUT = async (request) => {
+    try {
+        const {id} = request.params;
+        const { price, availability, brand, model, rating } = await request.json();
+        
+        const productUp = await DataBaseInteraction.Product.update({
+            where: {
+                id:id,
+            },
+            data: {
+                price: price,
+                availability: availability,
+                brand: brand,
+                model: model,
+                rating: rating,
+              },
+        })
+        return NextResponse.json(productUp,{status: 200})
+    } catch (error) {
+        return NextResponse.json({error: error.message},{status: 500})
+    }
+} 
+
+
+export const DELETE = async (request) => {
+    try {
+        const {id} = request.params;
+        const result = await DataBaseInteraction.Product.delete({
+            where: {
+                id: id, 
+            },
+        })
+        return NextResponse.json(result, {status: 200})
+    } catch (error) {
+        return NextResponse.json({message: error.message}, {status: 500,}); 
+    }
+} 
