4e50cbc99b38a371d03e877af3d8707da3e0a9e9
Jose G. Basanta <jgj1989@gmail.com>
2023-10-25 15:46:33 -0400

    Funcionalidad de Formulario para agregar productos

--- app/client/admin/productInputForm/productInputForm.component.jsx
+++ app/client/admin/productInputForm/productInputForm.component.jsx
@@ -1,42 +1,73 @@
 "use client";
 import { useState } from "react";
+import axios from "axios";
 export default function ProductInputForm() {
   const [product, setProduct] = useState({
-    name: "",
     price: "",
     availability: "",
     brand: "",
     model: "",
+    img: "",
   });
 
   const handleInputChange = (e) => {
-    setProduct({ ...product, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+
+    if (name === "price" || name === "availability") {
+      const parsedValue = parseInt(value, 10);
+      setProduct({ ...product, [name]: parsedValue });
+    } else {
+      setProduct({ ...product, [name]: value });
+    }
     console.log(product);
   };
-  const handleSelectBrand = (e) => {
-    setProduct({ ...product, brand: e.target.value });
+  //Funcion para subir imagenes a Cloudinary
+  const changeUploadImage = async (e) => {
+    const file = e.target.files[0];
+    const data = new FormData();
+    data.append("file", file);
+    data.append("upload_preset", "imagenesProductos");
+    const response = await axios.post(
+      "https://api.cloudinary.com/v1_1/dgmfqs7v9/image/upload",
+      data
+    );
+    setProduct({ ...product, img: response.data.secure_url });
+  };
+  //Eliminar Imagen en el front-end eliminando la URL del estado.
+  const deleteImage = () => {
+    setProduct({ ...product, img: "" });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     console.log(product);
+
+    // const productJSON = JSON.stringify(product);
+
+    await axios.post("http://localhost:3000/product/form/api", product);
+
+    alert("Producto Creado");
   };
   return (
     <div className="container ">
-      <form class="mx-auto">
-        <label class="flex mx-auto border justify-center">
+      <form className="mx-auto">
+        <label className="flex mx-auto border justify-center">
           Nombre del Producto
         </label>
         <input
-          class="mx-auto w-full justify-center bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 "
+          className="mx-auto w-full justify-center bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 "
           type="text"
           placeholder="'Bujia'"
           name="model"
           onChange={handleInputChange}
         ></input>
         <label className="flex mx-auto border justify-center">
-          Precio del Producto
+          Marca del Producto
         </label>
         <input
           className="mx-auto w-full justify-center bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5"
           type="text"
-          name="price"
+          name="brand"
           onChange={handleInputChange}
         ></input>
         <label className="flex mx-auto border justify-center">
@@ -49,19 +80,50 @@ export default function ProductInputForm() {
           onChange={handleInputChange}
         ></input>
         <label className="flex mx-auto border justify-center">
-          Marca del Producto
+          Precio del Producto
         </label>
         <input
           className="mx-auto w-full justify-center bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5"
           type="text"
-          name="brand"
+          name="price"
           onChange={handleInputChange}
         ></input>
-        <button className="flex m-4 justify-center border">Subir Imagen</button>
-        <button className="flex m-4 border">Borrar Imagen</button>
-        <button className="flex m-4 border">Enviar</button>
-        <button className="flex m-4 border">Cancelar</button>
+        <label className="flex mx-auto border justify-center">
+          Imagen del Producto:
+        </label>
+        <input
+          className="flex m-4 justify-center border"
+          type="file"
+          accept="image/*"
+          onChange={changeUploadImage}
+        />
+
+        <button
+          className="flex m-4 border"
+          type="submit"
+          onClick={() => handleSubmit()}
+        >
+          Enviar
+        </button>
+        <button className="flex m-4 border" onClick={() => deleteProductInfo()}>
+          Cancelar
+        </button>
       </form>
+      <div>
+        <p>Nombre: {product.model}</p>
+        <p>Marca: {product.brand}</p>
+        <p>Disponibilidad: {product.availability}</p>
+        <p>Precio: {product.price}</p>
+        {product.img.length > 0 ? (
+          <div>
+            <p>Imagen Cargada</p>
+            <img src={product.img} alt="" />
+            <button className="flex m-4 border" onClick={() => deleteImage()}>
+              Borrar Imagen
+            </button>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }
@@ -77,10 +139,9 @@ export default function ProductInputForm() {
 //[x] - hacer el boton de subir foto
 //[x] - hacer el boton de borrar foto
 //[x] - hacer el boton de cancelar
-//[] - crear los estados locales para guardar los inputs
-//[] - hacer que se reflejen los inputs en la pagina para que el usuario vea los datos
-//[] - configurar Cloudinary
-//[] - integrar cloudinary a la form
-//[] - integrar cloudinary a la base de datos
+//[x] - crear los estados locales para guardar los inputs
+//[x] - hacer que se reflejen los inputs en la pagina para que el usuario vea los datos
+//[x] - configurar Cloudinary
+//[x] - integrar cloudinary a la form
 //[] - codear la funcion para submit a la base de datos
 //[] - testear la info de la base de datos
