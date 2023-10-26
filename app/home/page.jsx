2a5f3dc5fe2ae82b575c8a4e994eed27251a1c55
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2023-10-26 14:39:54 -0500

    V30 - Ulises Landing Page

---
+++ app/home/page.jsx
@@ -0,0 +1,15 @@
+import { Footer, Navbar } from '@/components'
+import ProductsCards from '@/components/ProductsCards'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      
+      <ProductsCards/>
+      
+    </div>
+  )
+}
+
+export default Home
