d0c57b90814668732e14bde40f13c21d5bc56c67
Ulises Giordano <giordau@gmail.com>
2023-10-26 16:12:33 -0300

    v1 landing

---
+++ constants/index.js
@@ -0,0 +1,121 @@
+
+export const footerLinks = [
+  {
+    title: "About",
+    links: [
+      { title: "How it works", url: "/" },
+      { title: "Featured", url: "/" },
+      { title: "Partnership", url: "/" },
+      { title: "Bussiness Relation", url: "/" },
+    ],
+  },
+  {
+    title: "Company",
+    links: [
+      { title: "Events", url: "/" },
+      { title: "Blog", url: "/" },
+      { title: "Podcast", url: "/" },
+      { title: "Invite a friend", url: "/" },
+    ],
+  },
+  {
+    title: "Socials",
+    links: [
+      { title: "Discord", url: "/" },
+      { title: "Instagram", url: "/" },
+      { title: "Twitter", url: "/" },
+      { title: "Facebook", url: "/" },
+    ],
+  },
+];
+
+
+// Este es el array para hardcode ProductsCards
+export const parts = [
+  {
+    model: "piñon arranque",
+    brand: "zen",
+    price: "5200",
+    availability: "20",
+    img: "/img_parts/imagen1.jpg",
+  },
+  {
+    model: "rotor alternador",
+    brand: "Nashville",
+    price: "10000",
+    availability: "40",
+    img: "/img_parts/imagen2.jpg",
+  },
+  {
+    model: "pastilla de freno ",
+    brand: "Audi",
+    price: "20500",
+    availability: "10",
+    img: "/img_parts/imagen3.jpg",
+  },
+  {
+    model: "piñon arranque",
+    brand: "zen",
+    price: "5200",
+    availability: "20",
+    img: "/img_parts/imagen1.jpg",
+  },
+  {
+    model: "rotor alternador",
+    brand: "Nashville",
+    price: "10000",
+    availability: "40",
+    img: "/img_parts/imagen2.jpg",
+  },
+  {
+    model: "pastilla de freno ",
+    brand: "Audi",
+    price: "20500",
+    availability: "10",
+    img: "/img_parts/imagen3.jpg",
+  },
+  {
+    model: "piñon arranque",
+    brand: "zen",
+    price: "5200",
+    availability: "20",
+    img: "/img_parts/imagen1.jpg",
+  },
+  {
+    model: "rotor alternador",
+    brand: "Nashville",
+    price: "10000",
+    availability: "40",
+    img: "/img_parts/imagen2.jpg",
+  },
+  {
+    model: "pastilla de freno ",
+    brand: "Audi",
+    price: "20500",
+    availability: "10",
+    img: "/img_parts/imagen3.jpg",
+  },
+  {
+    model: "piñon arranque",
+    brand: "zen",
+    price: "5200",
+    availability: "20",
+    img: "/img_parts/imagen1.jpg",
+  },
+  {
+    model: "rotor alternador",
+    brand: "Nashville",
+    price: "10000",
+    availability: "40",
+    img: "/img_parts/imagen2.jpg",
+  },
+  {
+    model: "pastilla de freno ",
+    brand: "Audi",
+    price: "20500",
+    availability: "10",
+    img: "/img_parts/imagen3.jpg",
+  },
+
+
+]
