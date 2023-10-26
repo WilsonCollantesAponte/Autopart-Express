d89d207dbe17680ad9c2bc09752eeb2a2c00a7bc
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2023-10-26 10:40:59 -0500

    V27

--- app/client/form/login/api/name/route.js
+++ app/client/form/login/api/name/route.js
@@ -2,18 +2,18 @@ import DataBaseInteraction from "@/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
-  // try {
-  const { searchParams } = new URL(request.url);
-  let name = searchParams.get("name");
+  try {
+    const { searchParams } = new URL(request.url);
+    let name = searchParams.get("name");
 
-  const client = await DataBaseInteraction.client.findMany({
-    where: {
-      name: name,
-    },
-  });
-  // console.log (client)
-  return NextResponse.json({ client });
-  // } catch (error) {
-  //   return NextResponse.json({ message: error.message }, { status: 500 });
-  // }
+    const client = await DataBaseInteraction.client.findMany({
+      where: {
+        name: name,
+      },
+    });
+
+    return NextResponse.json({ client });
+  } catch (error) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
 }
