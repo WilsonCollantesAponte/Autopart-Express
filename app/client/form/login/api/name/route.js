3e38012f5571db0a13808c31e2afd995f6fc406e
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2023-10-26 11:04:13 -0500

    V27

--- app/client/form/login/api/name/route.js
+++ app/client/form/login/api/name/route.js
@@ -13,8 +13,6 @@ export async function GET(request) {
     });
 
     return NextResponse.json({ client });
-    {
-    }
   } catch (error) {
     return NextResponse.json({ message: error.message }, { status: 500 });
   }
