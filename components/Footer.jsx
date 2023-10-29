e1c40fc2e86e61db5265e41b53cab2ef5b4ec35c
claudiagranados <claudiagranados@outlook.com>
2023-10-29 15:20:06 -0500

    Estilos formulario registro-logo navbar

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,26 +2,26 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className="bg-gris-oscuro text-white">
+    <footer className="bg-gris-oscuro text-black w-screen">
       <div className="container mx-auto p-8 flex flex-col md:flex-row justify-between items-start">
       
         <div className="mb-4 md:mb-0">
-          <img src='/autoexpress-sinfondo.png' alt="Logo" className="h-16 w-auto" />
+          <img src='/autoexpress-sinfondo.png' alt="Logo" className="h-10 w-auto" />
         </div>
         
         <div className="mb-4 md:mb-0">
           <h3 className="text-lg font-semibold mb-2">INFORMACIÓN</h3>
-          <p>Acerca de <Link href="/acerca">Autopart-Express</Link></p>
+          <p><Link className="hover:text-gray-200" href="/acerca">Acerca de Autopart-Express</Link></p>
         </div>
         
         <div className="mb-4 md:mb-0">
           <h3 className="text-lg font-semibold mb-2">CONTACTO</h3>
-          <p><Link href="/contacto">Integrantes</Link></p>
+          <p><Link className="hover:text-gray-200" href="/contacto">Integrantes</Link></p>
         </div>
       </div>
       
     
-      <hr className="border-white border-t mx-auto w-4/5" />
+      <hr className="border-blue-Nav border-t mx-auto w-4/5" />
       
       <div className="text-center py-4">
         Autopart-Express 2023 Todos los derechos reservados &copy;
