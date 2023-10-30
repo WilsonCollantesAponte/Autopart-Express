da5013a7e4efc2998b985b3303e39c04bf399324
Jose G. Basanta <jgj1989@gmail.com>
2023-10-30 00:15:08 -0400

    Formulario de productos actualizado a la nueva version de las tablas de Base de Datos

--- app/dashboard/products/components/ProductForm.jsx
+++ app/dashboard/products/components/ProductForm.jsx
@@ -1,25 +1,25 @@
 "use client";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import axios from "axios";
 export default function ProductForm() {
+  const form = useRef();
   const [product, setProduct] = useState({
-    model: "",
-    brand: "",
+    name: "",
     price: "",
     availability: "",
-    img: "",
+    brand: "",
+    model: "",
+    image: "",
   });
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-
     if (name === "price" || name === "availability") {
       const parsedValue = parseInt(value, 10);
       setProduct({ ...product, [name]: parsedValue });
     } else {
       setProduct({ ...product, [name]: value });
     }
-    console.log(product);
   };
   //Funcion para subir imagenes a Cloudinary
   const changeUploadImage = async (e) => {
@@ -31,31 +31,48 @@ export default function ProductForm() {
       "https://api.cloudinary.com/v1_1/dgmfqs7v9/image/upload",
       data
     );
-    setProduct({ ...product, img: response.data.secure_url });
+    setProduct({ ...product, image: response.data.secure_url });
   };
   //Eliminar Imagen en el front-end eliminando la URL del estado.
   const deleteImage = () => {
-    setProduct({ ...product, img: "" });
+    setProduct({ ...product, image: "" });
   };
 
   const handleSubmit = async (e) => {
+    e.preventDefault();
     if (
       product.model === "" ||
       product.brand === "" ||
       product.price === 0 ||
       product.availability === 0 ||
-      product.img === ""
+      product.image === ""
     ) {
-      return alert("faltan datos"); // Exit the function if any fields are empty
+      return alert("faltan datos");
     }
-
-    await axios.post("http://localhost:3000/product/form/api", product);
-
+    await axios.post("http://localhost:3000/dashboard/products/api/", product);
     alert("Producto Creado");
+    setProduct({
+      name: "",
+      price: "",
+      availability: "",
+      brand: "",
+      model: "",
+      image: "",
+    });
   };
   return (
     <div className="container ">
-      <form className="mx-auto">
+      <form className="mx-auto" onSubmit={handleSubmit}>
+        <label className="flex mx-auto border justify-center">
+          Categoria del Producto
+        </label>
+        <input
+          className="mx-auto w-full justify-center bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 "
+          type="text"
+          placeholder="'Bujia'"
+          name="name"
+          onChange={handleInputChange}
+        ></input>
         <label className="flex mx-auto border justify-center">
           Nombre del Producto
         </label>
@@ -103,26 +120,22 @@ export default function ProductForm() {
           onChange={changeUploadImage}
         />
 
-        <button
-          className="flex m-4 border"
-          type="submit"
-          onClick={() => handleSubmit()}
-        >
+        <button className="flex m-4 border" type="submit">
           Enviar
         </button>
-        <button className="flex m-4 border" onClick={() => deleteProductInfo()}>
+        {/* <button className="flex m-4 border" onClick={() => deleteProductInfo()}>
           Cancelar
-        </button>
+        </button> */}
       </form>
       <div>
         <p>Nombre: {product.model}</p>
         <p>Marca: {product.brand}</p>
         <p>Disponibilidad: {product.availability}</p>
         <p>Precio: {product.price}</p>
-        {product.img.length > 0 ? (
+        {product.image.length > 0 ? (
           <div>
             <p>Imagen Cargada</p>
-            <img src={product.img} alt="" />
+            <img src={product.image} alt="" />
             <button className="flex m-4 border" onClick={() => deleteImage()}>
               Borrar Imagen
             </button>
@@ -132,25 +145,3 @@ export default function ProductForm() {
     </div>
   );
 }
-
-//To-Do:
-//[x] - escribir los <labels> de los inputs
-//  [x] - price
-//  [x] - availability
-//  [x] - brand
-//  [x] - model
-//[x] - codear los inputs
-//[x] - hacer el boton de submit
-//[x] - hacer el boton de subir foto
-//[x] - hacer el boton de borrar foto
-//[x] - hacer el boton de cancelar
-//[x] - crear los estados locales para guardar los inputs
-//[x] - hacer que se reflejen los inputs en la pagina para que el usuario vea los datos
-//[x] - configurar Cloudinary
-//[x] - integrar cloudinary a la form
-//[] - codear la funcion para submit a la base de datos
-//[] - testear la info de la base de datos
-
-// export default function ProductForm() {
-//   return <div> es</div>;
-// }
