6394ec5fe73baaaa9f8ce7756878216919d6d1b9
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2023-10-30 15:02:40 -0500

    V56 - Dashboard products - logical erase

--- app/dashboard/clients/page.jsx
+++ app/dashboard/clients/page.jsx
@@ -1,3 +1,72 @@
-export default function ClientDashboard() {
-  return <h1 className=" font-semibold text-2xl">Client Dashboard</h1>;
+"use client";
+
+import { useEffect, useState } from "react";
+import ClientRow from "./clientRow";
+
+export default function UsersDashborad() {
+  const [clients, setClients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("/dashboard/clients/api")
+      .then((r) => r.json())
+      .then((r) => setClients(r.allClients))
+      .then(() => setIsLoading(false));
+  }, []);
+
+  return (
+    <div>
+      {isLoading ? (
+        <div className=" m-9">
+          <div className=" grid grid-cols-6 animate-pulse gap-y-6 gap-x-3 p-4">
+            <div className="h-4 bg-slate-400/70 rounded-lg col-span-5"></div>
+            <div className="h-4 bg-slate-400/70 rounded-lg col-span-full"></div>
+            <div className="h-4 bg-slate-400/70 rounded-lg col-span-full"></div>
+            <div className="h-4 bg-slate-400/70 rounded-lg col-span-full"></div>
+            <div className="h-4 bg-slate-400/70 rounded-lg col-span-full"></div>
+            <div className="h-4 bg-slate-400/70 rounded-lg col-span-full"></div>
+            <div className="h-4 bg-slate-400/70 rounded-lg col-span-full"></div>
+          </div>
+        </div>
+      ) : clients ? (
+        <div className=" mx-6 mb-3 border-x-2 border-gray-500 text-lg mt-10 rounded">
+          <div className="flex divide-x-2 divide-gray-500 border-y-2 border-gray-500 font-semibold overflow-auto">
+            <div className="flex items-center">
+              <div className=" py-2.5 pl-4 w-52">Nombre(s)</div>
+            </div>
+            <div className="flex items-center">
+              <div className=" py-2.5 pl-4 w-52">Apellido(s)</div>
+            </div>
+            <div className="flex items-center">
+              <div className=" py-2.5 pl-4 w-72">Email</div>
+            </div>
+
+            <div className="flex items-center">
+              <div className=" pl-1.5 w-72">Contraseña</div>
+            </div>
+
+            <div className="flex items-center">
+              <div className=" py-2.5 pl-4 w-24">Status</div>
+            </div>
+            <div className="flex items-center">
+              <div className=" py-2.5 pl-4">Options</div>
+            </div>
+          </div>
+          {clients.map((value, index) => {
+            return (
+              <ClientRow
+                key={index}
+                clientValue={value}
+                setClients={setClients}
+                clients={clients}
+              />
+            );
+          })}
+        </div>
+      ) : (
+        <h1>Error, please try again or after {JSON.stringify(error)}</h1>
+      )}
+    </div>
+  );
 }
