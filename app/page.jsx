23fada0aadf73552d36ed3e5853f266065ed0da1
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2023-11-03 21:01:33 -0500

    V98 - Fixing cart

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,23 @@
+"use client";
+
 import { Hero } from "@/components";
-import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+// import Image from "next/image";
 
 export default function Home() {
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (session) {
+      localStorage.setItem("name", session.user.name);
+      localStorage.setItem("email", session.user.email);
+      localStorage.setItem("image", session.user.image);
+    }
+  }, [session?.user.email]);
+
   return (
     <main className="overflow-hidden">
-   
       <Hero />
     </main>
   );
