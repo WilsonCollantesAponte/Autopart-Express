a530d1a3107dddf05a838c9fb14b5f19bd2aca0b
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2023-11-05 10:51:55 -0500

    -

--- app/placeOrder/failure/page.jsx
+++ app/placeOrder/failure/page.jsx
@@ -1,31 +1,45 @@
-import React from 'react'
+"use client";
+
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react/cjs/react.production.min";
+
+// import React from 'react'
 
 function page() {
+  const { data: session } = useSession();
+
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    setEmail(session?.user.email || localStorage.getItem("email"));
+  }, [session?.user.email]);
+
   return (
     <div>
-         <section class="text-gray-600 body-font">
+      <section class="text-gray-600 body-font">
         <div class="container mx-auto flex flex-col px-5 py-24 justify-center items-center">
-         <div class="w-full md:w-2/3 flex flex-col mb-16 items-center text-center">
-         {
-          email? (
-            <div>
-              <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Compra fallida. {email}</h1>
-            </div>
-          ):(
-            <p>loading...</p>
-          )
-         }
-          
+          <div class="w-full md:w-2/3 flex flex-col mb-16 items-center text-center">
+            {email ? (
+              <div>
+                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+                  Compra fallida. {email}
+                </h1>
+              </div>
+            ) : (
+              <p>loading...</p>
+            )}
           </div>
-          <Link href='/home'>
+          <Link href="/home">
             <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
               vuelva a intentar
             </button>
           </Link>
-         </div>   
+        </div>
       </section>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
