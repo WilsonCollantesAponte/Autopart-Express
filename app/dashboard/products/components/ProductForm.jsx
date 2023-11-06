f865277b3b8540f102c75779cc613abb5a2ca9bd
Jose G. Basanta <jgj1989@gmail.com>
2023-11-05 20:18:06 -0400

    Animacion de carga para la imagen y el submit

--- app/dashboard/products/components/ProductForm.jsx
+++ app/dashboard/products/components/ProductForm.jsx
@@ -20,6 +20,9 @@ export default function ProductForm() {
     model: "Obligatorio",
   });
 
+  const [loadingImage, setLoadingImage] = useState(false);
+  const [submitingForm, setSubmitingForm] = useState(false);
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
 
@@ -97,6 +100,7 @@ export default function ProductForm() {
 
   //Funcion para subir imagenes a Cloudinary
   const changeUploadImage = async (e) => {
+    setLoadingImage(true);
     const file = e.target.files[0];
     const data = new FormData();
     data.append("file", file);
@@ -106,6 +110,7 @@ export default function ProductForm() {
       data
     );
     setProduct({ ...product, image: response.data.secure_url });
+    setLoadingImage(false);
   };
   //Eliminar Imagen en el front-end eliminando la URL del estado.
   const deleteImage = () => {
@@ -113,6 +118,7 @@ export default function ProductForm() {
   };
 
   const handleSubmit = async (e) => {
+    setSubmitingForm(true);
     e.preventDefault();
     if (!Object.keys(errorMessage).length > 0) {
       console.log(errorMessage);
@@ -135,6 +141,7 @@ export default function ProductForm() {
       brand: "Obligatorio",
       model: "Obligatorio",
     });
+    setSubmitingForm(false);
   };
 
   return (
@@ -226,15 +233,68 @@ export default function ProductForm() {
           />
           {!product.image && <p className="text-red-500">Obligatorio</p>}
 
+          {loadingImage && (
+            <div class="flex justify-center items-center">
+              <svg
+                class="animate-spin h-5 w-5 mr-3 text-gray-500"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  class="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"
+                ></circle>
+                <path
+                  class="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647zM20 12a8 8 0 01-8 8v4c6.627 0 12-5.373 12-12h-4zm-2-5.291A7.962 7.962 0 0120 12h4c0-3.042-1.135-5.824-3-7.938l-3 2.647z"
+                ></path>
+              </svg>
+              <span className="bold text-blue-500">
+                Cargando la imagen...Espere, por favor.
+              </span>
+            </div>
+          )}
+
           <div className="double-space"></div>
 
           <button
-            className="w-full bg-blue-Nav hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className="w-full bg-blue-Nav hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-slate-400"
             type="submit"
             disabled={Object.values(errorMessage).some((value) => value !== "")}
           >
             Enviar
           </button>
+          {submitingForm && (
+            <div class="flex justify-center items-center">
+              <svg
+                class="animate-spin h-5 w-5 mr-3 text-gray-500"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  class="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"
+                ></circle>
+                <path
+                  class="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647zM20 12a8 8 0 01-8 8v4c6.627 0 12-5.373 12-12h-4zm-2-5.291A7.962 7.962 0 0120 12h4c0-3.042-1.135-5.824-3-7.938l-3 2.647z"
+                ></path>
+              </svg>
+              <span>Enviando...</span>
+            </div>
+          )}
         </form>
       </div>
 
