ebaea034408442975bea77f204478200d4ff3e81
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2023-11-15 11:00:40 -0500

    V177

--- app/placeOrder/checkout/route.js
+++ app/placeOrder/checkout/route.js
@@ -1,45 +1,43 @@
 import mercadopago from "mercadopago";
-import { NextRequest , NextResponse } from "next/server";
-const {NEXT_ACCES_TOKEN} = process.env;
+import { NextRequest, NextResponse } from "next/server";
+const { NEXT_ACCES_TOKEN } = process.env;
 
 mercadopago.configure({
-    access_token: NEXT_ACCES_TOKEN,
+  access_token: NEXT_ACCES_TOKEN,
 });
 
 export async function POST(request) {
-    
-        try {
-            const productos = await request.json();
+  try {
+    const productos = await request.json();
 
-            console.log(productos)
-            const arrayProductos = productos.map(producto => {
-                return{
-                    id: producto.id,
-                    picture_url: producto.image,
-                    title: producto.name ,
-                    quantity: producto.quantity, 
-                    unit_price: producto.price, 
-                    currency_id: "ARG"
-                };
-            }) 
+    console.log(productos);
+    const arrayProductos = productos.map((producto) => {
+      return {
+        id: producto.id,
+        picture_url: producto.image,
+        title: producto.name,
+        quantity: producto.quantity,
+        unit_price: producto.price,
+        currency_id: "ARG",
+      };
+    });
 
-             const URL = "https://64e7-181-209-91-123.ngrok-free.app" //http://localhost3000 deveria https:// o el deploy 
-               
-            let preference = {
-     
-                        items: arrayProductos,
+    //  const URL = "https://64e7-181-209-91-123.ngrok-free.app" //http://localhost3000 deveria https:// o el deploy
+    const URL = "https://autopart-express.vercel.app"; //http://localhost3000 deveria https:// o el deploy
 
-                        back_urls: {
-                        failure: `${URL}/home`,
-                        pending: `${URL}/home`,
-                        success: `${URL}/placeOrder/status`,
-                        },
-                       notification_url: `${URL}/placeOrder/notify`, //crear el end point  
-                } 
-               const response = await mercadopago.preferences.create(preference);
-             return NextResponse.json({url: response.body.init_point});
-        } catch (error) {
-            return NextResponse.json({ message: error.message }, { status: 400 });
-        }
+    let preference = {
+      items: arrayProductos,
+
+      back_urls: {
+        failure: `${URL}/home`,
+        pending: `${URL}/home`,
+        success: `${URL}/placeOrder/status`,
+      },
+      notification_url: `${URL}/placeOrder/notify`, //crear el end point
+    };
+    const response = await mercadopago.preferences.create(preference);
+    return NextResponse.json({ url: response.body.init_point });
+  } catch (error) {
+    return NextResponse.json({ message: error.message }, { status: 400 });
+  }
 }
-
